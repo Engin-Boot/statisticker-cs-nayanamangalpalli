44f30171871ce65055aad96c20b8940bdb43fbaa
nayanamangalpalli <41451576+nayanamangalpalli@users.noreply.github.com>
2020-09-06 01:07:29 +0530

    Updated Methods

--- Statistics/Statistics.cs
+++ Statistics/Statistics.cs
@@ -13,10 +13,14 @@ namespace Statistics
                 stats.average = float.NaN;
                 stats.min = float.NaN;
                 stats.max = float.NaN;
+                
                 return stats;
             }
             else{
-                stats.CalculateAverageMinMax(numbers);
+                stats.CalculateAverage(numbers);
+                stats.CalculateMin(numbers);
+                stats.CalculateMax(numbers);
+                
                 return stats;
             }
             
@@ -28,19 +32,34 @@ namespace Statistics
         public float max;
         public float min;
         
-        public void CalculateAverageMinMax(List<float> numbers){
+        public void CalculateAverage(List<float> numbers){
+            
             float sum = numbers[0];
-            float min = numbers[0];
-            float max = numbers[0];
             for(int i = 1; i< numbers.Count; i++){
+                
                 sum = sum + numbers[i];
-                min = numbers[i] < min ? numbers[i] : min;
-                max = numbers[i] > max ? numbers[i] : max;
             }
             this.average = sum / numbers.Count;
+        }    
+         
+        public void CalculateMin(List<float> numbers){
+            
+            float min = numbers[0];
+            for(int i = 1; i< numbers.Count; i++){
+                
+                min = numbers[i] < min ? numbers[i] : min;
+            }
             this.min = min;
-            this.max = max;
+        }
         
+        public void CalculateMax(List<float> numbers){
+            
+            float max = numbers[0];
+            for(int i = 1; i< numbers.Count; i++){
+                
+                max = numbers[i] > max ? numbers[i] : max;
+            }
+            this.max=max;
         }
     }
     
