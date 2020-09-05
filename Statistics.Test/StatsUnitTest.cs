acf0955197216017767a6ebb5fa248624e4b8003
nayanamangalpalli <41451576+nayanamangalpalli@users.noreply.github.com>
2020-09-05 20:44:20 +0530

    Changes reference object

--- Statistics.Test/StatsUnitTest.cs
+++ Statistics.Test/StatsUnitTest.cs
@@ -13,18 +13,18 @@ namespace Statistics.Test
             var computedStats = statsComputer.CalculateStatistics(
                 new List<___>{1.5, 8.9, 3.2, 4.5});
             float epsilon = 0.001F;
-            Assert.True(Math.Abs(statsComputer.average - 4.525) <= epsilon);
-            Assert.True(Math.Abs(statsComputer.max - 8.9) <= epsilon);
-            Assert.True(Math.Abs(statsComputer.min - 1.5) <= epsilon);
+            Assert.True(Math.Abs(computedStats.average - 4.525) <= epsilon);
+            Assert.True(Math.Abs(computedStats.max - 8.9) <= epsilon);
+            Assert.True(Math.Abs(computedStats.min - 1.5) <= epsilon);
         }
-        public void ReportsNaNForEmptyInput()
-        {
-            var statsComputer = new StatsComputer();
-            var computedStats = statsComputer.CalculateStatistics(
-                new List<___>{});
-            //All fields of computedStats (average, max, min) must be
-            //Double.NaN (not-a-number), as described in
-            //https://docs.microsoft.com/en-us/dotnet/api/system.double.nan?view=netcore-3.1
-        }
+//         public void ReportsNaNForEmptyInput()
+//         {
+//             var statsComputer = new StatsComputer();
+//             var computedStats = statsComputer.CalculateStatistics(
+//                 new List<___>{});
+//             //All fields of computedStats (average, max, min) must be
+//             //Double.NaN (not-a-number), as described in
+//             //https://docs.microsoft.com/en-us/dotnet/api/system.double.nan?view=netcore-3.1
+//         }
     }
 }
