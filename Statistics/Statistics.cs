bf93f249c1dc97a60da314ea9b0d3edbbe782053
nayanamangalpalli <41451576+nayanamangalpalli@users.noreply.github.com>
2020-09-05 19:16:48 +0530

    Update Statistics.cs

--- Statistics/Statistics.cs
+++ Statistics/Statistics.cs
@@ -1,12 +1,27 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 namespace Statistics
 {
     public class StatsComputer
     {
+        float average;
+        float max;
+        float min;
+        
         public Stats CalculateStatistics(List<float> numbers) {
             //Implement statistics here
+            
+            float sum = 0;
+            float min = 0;
+            float max = 0;
+            foreach(float number in numbers){
+                sum = sum + number;
+                min = number < min ? number : min;
+                max = number < max ? number : max;
+            }
+            average = sum / numbers.Count;
+            
         }
     }
 }
